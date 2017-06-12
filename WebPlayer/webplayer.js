d40f3ac432013c7ef9fc44c3423837b2a6be1805
Frogge13 <fabianrogge@gmx.de>
2017-06-13 00:34:44 +0200

    play pause event

--- WebPlayer/webplayer.js
+++ WebPlayer/webplayer.js
@@ -4,19 +4,34 @@ class WebPlayer extends HTMLElement {
   }
   connectedCallback(){
     let playBtn = document.createElement("button");
+    let pauseBtn = document.createElement("button");
     let volumerange = document.createElement("input");
     let audio = document.createElement("audio");
 
     playBtn.innerText = "Play";
+    pauseBtn.innerText = "Pause";
     volumerange.type = "range";
     volumerange.min = 0;
     volumerange.max = 100;
+
+    playBtn.addEventListener("click", () => {
+      if (this.audio.paused) {
+        audio.play();
+      }
+    })
+    pauseBtn.addEventListener("click", () => {
+      if (!this.audio.paused) {
+        audio.pause();
+      }
+    })
+
     volumerange.addEventListener("change", () => {
       this.volumeChange();
     });
 
 
     this.appendChild(playBtn);
+    this.appendChild(pauseBtn);
     this.appendChild(volumerange);
     this.appendChild(audio);
 
