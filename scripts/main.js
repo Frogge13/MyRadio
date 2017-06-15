456cd0ffd9c505321b82d01f3bc6937b1908a81d
Frogge13 <fabianrogge@gmx.de>
2017-06-15 07:32:16 +0200

    final

--- scripts/main.js
+++ scripts/main.js
@@ -5,30 +5,31 @@ window.addEventListener("load", () => {
   let contentdiv = document.getElementById("content");
   let searchdiv = document.getElementById("search");
 
+  //----------------Select für Genre-------------------------------------------
   let select_genre = document.createElement("SELECT");
   let xhr_genre = new XMLHttpRequest();
   let dirble_genre = "?dirbleRequest=http://api.dirble.com/v2/categories";
   let defaultoptiongenre = document.createElement("option");
-  defaultoptiongenre.text = "---";
 
+  //----------------Select für Countries---------------------------------------
   let select_cntry = document.createElement("SELECT");
   let xhr_cntry = new XMLHttpRequest();
   let dirble_cntry = "?dirbleRequest=http://api.dirble.com/v2/countries";
   let defaultoptioncntry = document.createElement("option");
-  defaultoptioncntry.text = "---";
 
+  defaultoptiongenre.text = "---";
+  defaultoptioncntry.text = "---";
   select_cntry.add(defaultoptioncntry);
   select_genre.add(defaultoptiongenre);
 
   let xhr_search = new XMLHttpRequest();
 
+//------------------- Requests für alle Länder und Genres-----------------
   xhr_genre.open("GET", borstiUrl+dirble_genre);
   xhr_genre.send();
 
   xhr_cntry.open("GET", borstiUrl+dirble_cntry);
   xhr_cntry.send();
-
-
 
   xhr_genre.onreadystatechange = () => {
     if (xhr_genre.readyState === 4) { // DONE
@@ -66,13 +67,13 @@ window.addEventListener("load", () => {
       }
     }
   }
+  //----------------- Selects anzeigen------------------------------------------
   searchdiv.appendChild(select_genre);
   searchdiv.appendChild(select_cntry);
 
+  //---------------- EventListener für die Select Suche-------------------------
   select_genre.addEventListener("change", (e) => {
-
-    select_cntry.value = "---";
-
+    select_cntry.value = "---"; //Wird auf Default gesetzt, da nur nach einem Kriterum gesucht werden kann
     if (select_genre.value != "---") {
       xhr_search.open("GET", borstiUrl+"?dirbleRequest=http://api.dirble.com/v2/category/"+select_genre.options[select_genre.selectedIndex].dataset.id+"/stations");
       xhr_search.send();
@@ -87,9 +88,7 @@ window.addEventListener("load", () => {
   });
 
   select_cntry.addEventListener("change", (e) => {
-
-    select_genre.value = "---";
-
+    select_genre.value = "---"; //Wird auf Default gesetzt, da nur nach einem Kriterum gesucht werden kann
     if (select_cntry.value != "---") {
       xhr_search.open("GET", borstiUrl+"?dirbleRequest=http://api.dirble.com/v2/countries/"+select_cntry.options[select_cntry.selectedIndex].dataset.country_code+"/stations");
       xhr_search.send();
@@ -103,93 +102,63 @@ window.addEventListener("load", () => {
     }
 
   });
-
-  document.getElementById("searchBtn").addEventListener("click", () => {
-
-    xhr_search.open("GET", borstiUrl+"?dirbleRequest=http://api.dirble.com/v2/stations/popular");
-    xhr_search.send();
-
-    xhr_search.onreadystatechange = () => {
-      if (xhr_search.readyState === 4) {
-        if (xhr_search.status === 200) {
-          let searchObj = JSON.parse(xhr_search.responseText);
-          let div_array = [];
-          for (var i in searchObj) {
-            {
-              div_array[i] = document.createElement("div");
-              let img = document.createElement("img");
-              img.classList.add("thumbnail");
-              img.src = ""+searchObj[i].image.thumb.url+"";
-              img.alt = "FEHLER";
-              div_array[i].appendChild(img);
-              contentdiv.appendChild(div_array[i]);
-            }
-          }
-        }
-      }
-    }
-  });
-
+//---- Allgemeine Funktion um StationObjekte im Content anzuzeigen------------
   function showStations(stationObject){
-
-    contentdiv.innerHTML = "";
-    let end = 0;
-    console.log(stationObject);
+    contentdiv.innerHTML = ""; // Contentbereich leeren
     for (var i in stationObject) {
       {
+        //---------------- Elemente zur Visualisierung der Stations------------
         let wrapper = document.createElement("div");
-        wrapper.dataset.object = JSON.stringify(stationObject[i]);
         let img = document.createElement("img");
-        img.src = ""+stationObject[i].image.thumb.url+"";
-        img.alt = "FEHLER";
-        img.classList.add("thumbnail");
         let spanName = document.createElement("span");
-        spanName.innerText = stationObject[i].name;
         let spanCntry = document.createElement("span");
-        spanCntry.innerText = stationObject[i].country;
         let spanGenre = document.createElement("span");
+        let end; //wird benötigt um zu viele Kategorien auf ein minimun zu reduzieren
         if (stationObject[i].categories.length > 3) {
           end = 3;
         } else {
           end = stationObject[i].categories.length;
         }
+        let streamBtn = document.createElement("Button");
+        let select_fav = document.createElement("select"); // Auswahl mit allen Favoritenlisten
+        let defaultoptionfav = document.createElement("option");
+        //---------------- Elemente mit Werten füllen ------------------------
+        wrapper.dataset.object = JSON.stringify(stationObject[i]); //Das gesamte StationObject wird zwischengespeichert für die spätere Verwendung
+        img.src = ""+stationObject[i].image.thumb.url+"";
+        //img.alt = "noImage.png";
+        img.classList.add("thumbnail"); // Das Thumbnail wird auf 100x100px beschränkt
+        spanName.innerText = stationObject[i].name;
+        spanCntry.innerText = stationObject[i].country;
         for (let j = 0; j < end; j++) {
           spanGenre.innerText += stationObject[i].categories[j].title+", ";
         }
-        let streamBtn = document.createElement("Button");
         streamBtn.innerText = "Play";
-        streamBtn.addEventListener("click", (e) => {
-          document.getElementById("audioelement").src = JSON.parse(e.path[1].dataset.object).streams[0].stream;
-          console.log(e.path[1].dataset.object);
-          document.getElementById("audioelement").play();
-        });
-
-        let select_fav = document.createElement("select");
-        let defaultoptionfav = document.createElement("option");
-
         defaultoptionfav.text = "Zur Liste hinzufügen";
         select_fav.add(defaultoptionfav);
-
         for (let k = 0; k < document.getElementById("favoritenListe").getElementsByTagName("li").length; k++) {
           let option = document.createElement("option");
           option.text = document.getElementById("favoritenListe").getElementsByTagName("li")[k].innerText;
           select_fav.add(option);
         }
-
+//------------- Event um einen Stream zu spielen------------------------------------------------------------------------
+        streamBtn.addEventListener("click", (e) => {
+          document.getElementById("audioelement").src = JSON.parse(e.path[1].dataset.object).streams[0].stream;
+          document.getElementById("audioelement").play();
+        });
+//------------- Event um eine Station in einem Favoriten zu speichern --------------------------------------------------
         select_fav.addEventListener("change", (e) => {
           let liElement = document.getElementById("favoritenListe").getElementsByTagName("li")[e.srcElement.options.selectedIndex-1]; //li-Element identifizieren
-
           if (liElement.dataset.favObject == undefined) { //undefined muss überschrieben werden
             let array = [];
-            array[0] = JSON.parse(e.path[1].dataset.object);
-            liElement.dataset.favObject = JSON.stringify(array);
-          }else {
+            array[0] = JSON.parse(e.path[1].dataset.object); // der Dataset aus dem entsprechenden Wrapper-Div wird als Object gespeichert
+            liElement.dataset.favObject = JSON.stringify(array); // das Array wird im FavoritenListenElement in der Sidebar gespeichert
+          }else { // Hier werden die weiteren Stations als Objecte hinzugefügt
             let array = JSON.parse(liElement.dataset.favObject);
             array[array.length] = JSON.parse(e.path[1].dataset.object);
             liElement.dataset.favObject = JSON.stringify(array);
           }
         });
-
+//-------------- Elemente anzeigen ---------------------------------------
         wrapper.appendChild(img);
         wrapper.appendChild(spanName);
         wrapper.appendChild(spanCntry);
@@ -200,14 +169,17 @@ window.addEventListener("load", () => {
       }
     }
   }
-
+//-------------------EventListener um eine Liste hinzuzufügen--------------
   document.getElementById("addListBtn").addEventListener("click", () =>{
     let favName = prompt("Bitte Namen für neue Favoritenliste eingeben")
     if (favName != "" && favName != null) {
       let favItem = document.createElement("li");
       favItem.innerText = favName;
+      //------------- Eventlistener um die Favoritenliste anzuzuzeigen------
+      favItem.addEventListener("click", (e) => {
+        showStations(JSON.parse(e.target.dataset.favObject));
+      });
       document.getElementById("favoritenListe").appendChild(favItem);
     }
   });
-
 });
